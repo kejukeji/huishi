30b4b67986170cb58bf1c7d7880ee965b4478a38
binfreedom <250440083@qq.com>
2014-02-09 18:46:40 +0800

    huishi

---
+++ js/Untitled-1_edge.js
@@ -0,0 +1,70 @@
+/**
+ * Adobe Edge: symbol definitions
+ */
+(function($, Edge, compId){
+//images folder
+var im='images/';
+
+var fonts = {};
+
+
+var resources = [
+];
+var symbols = {
+"stage": {
+   version: "2.0.1",
+   minimumCompatibleVersion: "2.0.0",
+   build: "2.0.1.268",
+   baseState: "Base State",
+   initialState: "Base State",
+   gpuAccelerate: false,
+   resizeInstances: false,
+   content: {
+         dom: [
+         {
+            id:'Rectangle',
+            type:'rect',
+            rect:['49px','148px','62px','63px','auto','auto'],
+            fill:["rgba(192,192,192,1)"],
+            stroke:[0,"rgba(0,0,0,1)","none"]
+         }],
+         symbolInstances: [
+
+         ]
+      },
+   states: {
+      "Base State": {
+         "${_Stage}": [
+            ["color", "background-color", 'rgba(255,255,255,1)'],
+            ["style", "width", '550px'],
+            ["style", "height", '400px'],
+            ["style", "overflow", 'hidden']
+         ],
+         "${_Rectangle}": [
+            ["style", "left", '49px']
+         ]
+      }
+   },
+   timelines: {
+      "Default Timeline": {
+         fromState: "Base State",
+         toState: "",
+         duration: 1000,
+         autoPlay: true,
+         timeline: [
+            { id: "eid1", tween: [ "style", "${_Rectangle}", "left", '419px', { fromValue: '49px'}], position: 10, duration: 990 }         ]
+      }
+   }
+}
+};
+
+
+Edge.registerCompositionDefn(compId, symbols, fonts, resources);
+
+/**
+ * Adobe Edge DOM Ready Event Handler
+ */
+$(window).ready(function() {
+     Edge.launchComposition(compId);
+});
+})(jQuery, AdobeEdge, "EDGE-9842862");
---
+++ js/index.js
@@ -0,0 +1,37 @@
+(function(){
+    $(function(){
+        //star
+        var stars = $('.star');
+        setInterval(function(){
+            //toggle class
+            //stars.toggleClass('on');
+        },1000);
+
+
+        //animate
+        function drop(el,top,cb){
+            el.animate({
+                top:top
+            },500,'ease-in',function(){
+                el.animate({
+                    top:top-20
+                },200,'linear',function(){
+                    el.animate({
+                        top:top
+                    },200,'linear',function(){
+                        cb && cb();
+                    });
+                });
+            });
+        }
+
+        function dropNext(i){
+            drop(stars.eq(i),100*(i+1)+100,i<stars.length?function(){
+                dropNext(i+1);
+            }:null);
+        }
+        dropNext(0);
+    });
+
+
+})();
